1014ae686e6afe0966d6f0fe31d83cb537770b6d
paperboygold <lucasrw@protonmail.com>
2022-06-12 19:32:07 +1000

    Added comments and edited README

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 # go-reverse-shell
+This is a Go program which will create a reverse shell by connecting to a specified address and port. It was originally created by ILightThings (see below) and was edited by me to add platform-specific functionality.
 
+## Usage
+Edit line 16 in shell.go to use the IP address and port of your listener. Compile and then execute the program from the target machine to have it establish a session with you.
+
+Compiling for Windows: GOOS=windows go build shell.go
+
+Compiling for Linux: GOOS=linux go build shell.go
+
+## Credit
 Originally created by:
 https://github.com/ILightThings/
--- shell.go
+++ shell.go
@@ -1,8 +1,11 @@
 /*
-This is a Go program which will create a reverse shell by connecting to a specified address and port.
+This is a Go program which will create a reverse shell by connecting to a specified address and port. It can be used to create both Windows and Linux reverse shells.
 
-Usage: Edit line 16 in shell.go to use the IP address and port of your listener.
-Compile and then execute the program from the target machine to have it establish a session with you.
+Usage: Edit line 16 in shell.go to use the IP address and port of your listener. Compile and then execute the program from the target machine to have it establish a session with you.
+
+Compiling for Windows: GOOS=windows go build shell.go
+
+Compiling for Linux: GOOS=linux go build shell.go
 */
 
 package main
